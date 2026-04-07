eddc38547088931c4a24208d996c47165df8c749
Rafa <rafaelmejia2005@gmail.com>
2026-04-07 12:42:04 -0600

    Initial commit

---
+++ Cubiko/CubikoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubikoApp.swift
+//  Cubiko
+//
+//  Created by Rafael on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CubikoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
