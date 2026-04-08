07b33dce62dfb82890a4a17b75ea8286dfd7d8b3
Rafa <rafaelmejia2005@gmail.com>
2026-04-07 21:49:23 -0600

    Feature: Especific date v1

---
+++ Cubiko/Cubiculo.swift
@@ -0,0 +1,14 @@
+//
+//  Cubiculo.swift
+//  Cubiko
+//
+//  Created by Rafael on 07/04/26.
+//
+
+import Foundation
+
+struct Cubiculo: Identifiable {
+    let id: Int
+    let nombre: String
+    let tipo: String
+}
